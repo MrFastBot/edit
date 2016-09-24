af393baa07e2596ad904ddbfd3f5656cbc739f07
MrFastBot <MrFastBot@gmail.com>
2016-09-24 18:05:18 +0330

    Update index.php

--- index.php
+++ index.php
@@ -32,14 +32,24 @@ if (isset($update->edited_message)){
   $edname = $editm->from->first_name;
   $jsu = json_decode(file_get_contents(__DIR__.'/users/'.$eid.'.json'));
   $text = "<i>Dear </i><b>".$edname."</b>\n<code>I Saw What You Said</code>
-  <i>You Said</i>:
-  <b>".$jsu."</b>";
+<i>You Said</i>:
+<b>".$jsu."</b>";
   $id = $update->edited_message->chat->id;
   bot('sendmessage',[
     'chat_id'=>$id,
     'reply_to_message_id'=>$eid,
     'text'=>$text,
     'parse_mode'=>'html'
+    'reply_markup'=>json_encode([
+      'inline_keyboard'=>[
+        [
+          ['text'=>'Add To Group','url'=>'https://telegram.me/DoNotEditBot?startgroup=new']
+        ],
+        [
+          ['text'=>'Admin','text_2'=>'@IcePoker']
+        ]
+      ]
+    ])
   ]);
   $file_o = __DIR__.'/users/'.$eid.'.json';
   file_put_contents($file_o,json_encode($update->edited_message->text));
@@ -54,7 +64,7 @@ if (isset($update->edited_message)){
     'reply_markup'=>json_encode([
       'inline_keyboard'=>[
         [
-          ['text'=>'Ice Poker','url'=>'https://telegram.me/IcePker']
+          ['text'=>'Ice Poker','url'=>'https://telegram.me/IcePoker']
         ],
         [
           ['text'=>'Add To Group','url'=>'https://telegram.me/DoNotEditBot?startgroup=new']
